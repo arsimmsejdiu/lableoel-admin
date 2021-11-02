7de447232cf1a83f252ef5d29c74712f56ed53f5
arsimmsejdiu <arsimmsejdiu@gmail.com>
2021-11-02 15:33:12 +0100

    created the glog post in dashboard

--- components/Blog.jsx
+++ components/Blog.jsx
@@ -6,13 +6,18 @@ function Blog({ url, postImageSrc, title, description, authorName }) {
       <a className="post" href={url}>
         <Image height={256} width={256} className="post-image" src={postImageSrc}/>
         <div className="pl-8 pr-5">
-            <h6 className="post-title hover:text-purple-500 mt-5">{title}</h6>
+            <h2 className="post-title hover:text-purple-500 mt-5">{title}</h2>
             <p className="mt-4 text-sm font-semibold text-gray-600 leading-relaxed">{description}</p>
             <div className="mt-8 flex items-center">
                 <div className="text-xs text-gray-600">
                     <div className="text-gray-700 text-sm font-semibold mt-2 mb-5">{authorName}</div>
                 </div>
             </div>
+            <div className="flex justify-between mb-5">
+                <button className="py-3 px-6 text-white rounded-lg bg-yellow-500 shadow-lg block md:inline-block">Edit</button> 
+                <button className="py-3 px-6 text-white rounded-lg bg-red-500 shadow-lg block md:inline-block">Delete</button> 
+            </div>
+            
         </div>
       </a>
     </div>
